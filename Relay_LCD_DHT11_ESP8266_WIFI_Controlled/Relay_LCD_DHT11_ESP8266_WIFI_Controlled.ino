20559cfd046b210b29f0b600179cf98b8fcb9704
Will <62214186+Glliw@users.noreply.github.com>
2021-02-28 19:07:45 -0600

    Add files via upload
    fixed temp control.  started using functions that are called instead of in main loop function.  still need to resolve issues with "buttoncontrol".  function starts the heater, but doesn't turn off.

--- Relay_LCD_DHT11_ESP8266_WIFI_Controlled/Relay_LCD_DHT11_ESP8266_WIFI_Controlled.ino
+++ Relay_LCD_DHT11_ESP8266_WIFI_Controlled/Relay_LCD_DHT11_ESP8266_WIFI_Controlled.ino
@@ -5,17 +5,18 @@
 // SCL - > SCL
 // SDA - > SDA
 
-// WIFI control is through the Blynk IOT app.
+// WIFI control is through the "Blynk" IOT app.
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h> //Reference doc: https://www.arduino.cc/en/Reference/LiquidCrystalConstructor
 #include <DHT.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 
-#define ENABLE 14
+#define EnableHeater 14
 #define DHTPIN 3
 #define DHTTYPE DHT11
 #define BLYNK_PRINT Serial
+#define BLYNK_DEBUG
 
 LiquidCrystal_I2C  lcd(0x27, 2, 1, 0, 4, 5, 6, 7); // 0x27 is the I2C bus address of LCD backpack by default
 DHT dht(DHTPIN, DHTTYPE);
@@ -28,7 +29,7 @@ char pass[] = "scooter1";
 
 
 unsigned long Previous_Relay_Millis = 0;  // this variable will store the last time the relay was fired
-const long Relay_Interval = 1000; // sets the delay before opening / closing the relay again to prevent nuisance signals.  20,000 = 5 seconds
+const long Relay_Interval = 250000; // sets the delay before opening / closing the relay again to prevent nuisance signals.  250,000 = 250 seconds (4.66 min)
 unsigned long Previous_LCD_Millis = 0; // store the last time the LCD display updated
 const long LCD_Interval = 5000;
 unsigned long Previous_Serial_Millis = 0; // store the last time the LCD display updated
@@ -48,17 +49,55 @@ unsigned long ButtonOnTime;
 unsigned long ButtonPressedStartTime;
 String ControlStatus;
 unsigned long Previous_HTRButton_Millis;
-const long HTRButton_Interval = 3600000; // 1 hour
+const long HTRButton_Interval = 36000; // 1 hour = 3600000 ms
+int int_humidity;
+float heat_index_F;
+float heat_index_C;
+int ForceOnButton;
 
-BLYNK_CONNECTED() {
-  Blynk.syncVirtual(V0, V8); // syncs the button push for forced on mode and the slider position
+void GetDHT11() {
+  unsigned long Current_DHT11_Millis = millis();
+  if (Current_DHT11_Millis - Previous_DHT11_Millis >= DHT11_Interval) {
+    Previous_DHT11_Millis = Current_DHT11_Millis;  // this if statement "saves" the last time the action was taken.
+    humidity = dht.readHumidity();
+    tempC = dht.readTemperature();
+    tempF = dht.readTemperature(true); //(isFahrenheit = true)
+    Blynk.virtualWrite(V0, HeatSetPoint);
+    Serial.print("Control Status: ");
+    Serial.println(ControlStatus);
+  }
+
+  // Check if reading failed and exit early to try again.
+  if (isnan(humidity) || isnan(tempC) || isnan(tempF)) {
+    unsigned long Current_DHT11Timeout_Millis = millis();
+    if (Current_DHT11Timeout_Millis - Previous_DHT11Timeout_Millis >= DHT11Timeout_Interval) {
+      Previous_DHT11Timeout_Millis = Current_DHT11Timeout_Millis;  // this if statement "saves" the last time the action was taken.
+      Serial.println(F("Failed to receive a reading from the DHT11 sensor."));
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("NO DATA FROM ");
+      lcd.setCursor(0, 1);
+      lcd.print("SENSOR. RETRY.");
+      HeatSetPoint = 35;
+      Blynk.virtualWrite(V1, "Heater Off via slider - No data from sensor."); //sends status of the relay to Blynk
+      tempcontrolled();
+    }
+    return;
+  }
+  //Compute the heat index in Fahrenheit (the default)
+  heat_index_F = dht.computeHeatIndex(tempF, humidity);
+  //Compute the heat index in Celsius (isFahrenheit = False)
+  heat_index_C = dht.computeHeatIndex(tempC, humidity, false);
+  int_humidity = int(humidity);
+  return;
 }
 
-// define the slider input.
 
+//uses slider for temperature control
 void tempcontrolled() {
   ControlStatus = "Temperature Controlled";
-  Serial.print("The heater is now controlled by temperature.");
+  Serial.println("The heater is now controlled by temperature.");
+  Blynk.virtualWrite(V3, "Temperature Controlled");
   ButtonPressedStartTime = millis();
   unsigned long Current_Relay_Millis = millis();
   if (Current_Relay_Millis - Previous_Relay_Millis >= Relay_Interval) {
@@ -67,28 +106,35 @@ void tempcontrolled() {
       Serial.print("The V8 Button set point is: ");
       Serial.println(V8Status);
       Serial.println("The temp is too low.  Turn on Heater.");
-      digitalWrite(ENABLE, HIGH); //sends status of the relay to Blynk
+      digitalWrite(EnableHeater, HIGH); //sends status of the relay to Blynk
       Blynk.virtualWrite(V1, "Heater On");
-      Blynk.virtualWrite(V8, 0);
-      tempcontrolled();
+      Blynk.virtualWrite(V8, V8Status);
+      BLYNK_CONNECTED();
+      return;
     }
-    else if (tempF > HeatSetPoint) {
+    else if (tempF >= HeatSetPoint) {
       Serial.print("The V8 Button set point is: ");
       Serial.println(V8Status);
       Serial.println("The temp is too high.  Turn off the Heater.");
       //      TotalHeaterOnTime = TotalHeaterOnTime + LastHeaterOnTime;
-      digitalWrite(ENABLE, LOW);
-      Blynk.virtualWrite(V8, 0);
+      digitalWrite(EnableHeater, LOW);
+      Blynk.virtualWrite(V8, V8Status);
       Blynk.virtualWrite(V1, "Heater Off"); //sends status of the relay to Blynk
-      tempcontrolled();
+      BLYNK_CONNECTED();
+      return;
     }
+    else {
+      return;
+    }
+    return;
   }
-
+  return;
 }
 void buttoncontrolled() {
   ControlStatus = "Button Controlled";
   Serial.println("The Heater has been turned on by the forced ON command.");
-  digitalWrite(ENABLE, HIGH);
+  Blynk.virtualWrite(V3, "Button Controlled");
+  digitalWrite(EnableHeater, HIGH);
   Blynk.virtualWrite(V1, "Heater On");
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -109,10 +155,13 @@ void buttoncontrolled() {
     if (tempF > HeatSetPoint) {
       Blynk.virtualWrite(V8, 0);
       Blynk.virtualWrite(V1, "Heater Off");
-      digitalWrite(ENABLE, LOW);
+      digitalWrite(EnableHeater, LOW);
+      BLYNK_CONNECTED();
       tempcontrolled();
+
     }
     else {
+      BLYNK_CONNECTED();
       tempcontrolled();
     }
     lcd.clear();
@@ -122,13 +171,20 @@ void buttoncontrolled() {
     lcd.print("CONTROLLED");
     delay(3000);
   }
+  return;
+}
+
+BLYNK_CONNECTED() {
+  Blynk.syncVirtual(V0, V8); // syncs the button push for forced on mode and the slider position
 }
 
 BLYNK_WRITE(V0) {
   HeatSetPoint = param.asInt();
   Serial.print("V0 Slider value is: ");
   Serial.println(HeatSetPoint);
+  V8Status = 0;
   tempcontrolled();
+  return;
 }
 
 BLYNK_WRITE(V8) {
@@ -137,11 +193,13 @@ BLYNK_WRITE(V8) {
     buttoncontrolled();
   }
   else if (V8Status == 0) {
+    digitalWrite(EnableHeater, LOW); 
+    Blynk.virtualWrite(V1, "Heater Off");
     tempcontrolled();
   }
   Serial.print("V8 Button value is: ");
   Serial.println(V8Status);
-
+  return;
 }
 
 
@@ -152,7 +210,7 @@ void setup() {
   lcd.setBacklightPin(3, POSITIVE);
   lcd.setBacklight(HIGH);
   //lcd.clear();
-  pinMode(ENABLE, OUTPUT);
+  pinMode(EnableHeater, OUTPUT);
   lcd.setCursor(0, 0);
   lcd.print("Starting the");
   lcd.setCursor(0, 1);
@@ -166,42 +224,11 @@ void setup() {
 
 
 void loop() {
+  GetDHT11();
   Blynk.run();
   timer.run();
+  //tempcontrolled();
 
-  // delay (500); //sensor is slow to react; readings take ~0.25s each and data is ~2s old
-  unsigned long Current_DHT11_Millis = millis();
-  if (Current_DHT11_Millis - Previous_DHT11_Millis >= DHT11_Interval) {
-    Previous_DHT11_Millis = Current_DHT11_Millis;  // this if statement "saves" the last time the action was taken.
-    humidity = dht.readHumidity();
-    tempC = dht.readTemperature();
-    tempF = dht.readTemperature(true); //(isFahrenheit = true)
-    Blynk.virtualWrite(V0, HeatSetPoint);
-    Serial.print("Control Status: ");
-    Serial.println(ControlStatus);
-  }
-
-  // Check if reading failed and exit early to try again.
-  if (isnan(humidity) || isnan(tempC) || isnan(tempF)) {
-    unsigned long Current_DHT11Timeout_Millis = millis();
-    Serial.println(F("Failed to receive a reading from the DHT11 sensor."));
-    if (Current_DHT11Timeout_Millis - Previous_DHT11Timeout_Millis >= DHT11Timeout_Interval) {
-      Previous_DHT11Timeout_Millis = Current_DHT11Timeout_Millis;  // this if statement "saves" the last time the action was taken.
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("NO DATA FROM ");
-      lcd.setCursor(0, 1);
-      lcd.print("SENSOR. RETRY.");
-      HeatSetPoint = 40;
-      Blynk.virtualWrite(V1, "Heater Off via slider - No data from sensor."); //sends status of the relay to Blynk
-    }
-    return;
-  }
-  //Compute the heat index in Fahrenheit (the default)
-  float heat_index_F = dht.computeHeatIndex(tempF, humidity);
-  //Compute the heat index in Celsius (isFahrenheit = False)
-  float heat_index_C = dht.computeHeatIndex(tempC, humidity, false);
-  int int_humidity = int(humidity);
 
   unsigned long Current_Serial_Millis = millis();
   if (Current_Serial_Millis - Previous_Serial_Millis >= Serial_Interval) {
@@ -216,6 +243,11 @@ void loop() {
     Serial.print(F("Heat Index: "));
     Serial.print(heat_index_F);
     Serial.println(F("°F"));
+    Serial.print("Heater Relay Status: ");
+    Serial.println(digitalRead(EnableHeater));
+    Serial.print("Heater Set point: ");
+    Serial.print(HeatSetPoint);
+    Serial.println("F");
     Blynk.virtualWrite(V5, int_humidity);
     Blynk.virtualWrite(V6, tempF);
     Blynk.virtualWrite(V7, heat_index_F);
@@ -250,8 +282,4 @@ void loop() {
     lcd.print(heat_index_F);
     lcd.print("F");
   }
-
-  // this section fires the relay to enable the heater based on the set point of the Blynk app's V1 slider.
-
-
 }
